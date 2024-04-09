d4e23aeabf2947f8963cdb39a351287a0fa1bc2e
ReinstatedSamit <69500663+ReinstatedSamit@users.noreply.github.com>
2024-04-09 15:07:05 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -6,8 +6,8 @@
 module tb ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave.
- / initial begin
-  /  $dumpfile("tb.vcd");
+  initial begin
+    $dumpfile("tb.vcd");
    // $dumpvars(0, tb);
     #1;
   end
