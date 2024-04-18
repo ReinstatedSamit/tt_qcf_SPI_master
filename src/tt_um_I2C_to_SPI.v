bbb595f4dae6e6f939e843ea13636fa2a7192956
ReinstatedSamit <69500663+ReinstatedSamit@users.noreply.github.com>
2024-04-18 18:48:51 -0400

    Update tt_um_I2C_to_SPI.v

--- src/tt_um_I2C_to_SPI.v
+++ src/tt_um_I2C_to_SPI.v
@@ -18,6 +18,11 @@ reg temp_data_out;
 reg temp_clk_out;
 reg temp_data_oe;
 reg temp_clk_oe;
+reg temp_data_out2;
+reg temp_clk_out2;
+reg temp_data_oe2;
+reg temp_clk_oe2;
+
 assign MOD_bi = ui_in[3];
 /*  tt_um_I2C_SPI_Wrapper wrapper_inst(
     .i2c_data_in(ui_in[0]),
@@ -54,35 +59,41 @@ assign MOD_bi = ui_in[3];
         .i2c_data_oe(uio_oe[0]),
         .i2c_clk_oe(uio_oe[1]) */
   // All output pins must be assigned. If not used, assign to 0.
+
+        assign uio_out[0] = temp_data_out2;
+        assign uio_out[1] = temp_clk_out2;
+        assign uio_oe[0] = temp_data_oe2;
+        assign uio_oe[1] = temp_clk_oe2;
+		
+		assign uo_out[2] = temp_data_out;
+        assign uo_out[3] = temp_clk_out;
+        assign uo_out[4] = temp_data_oe;
+        assign uo_out[5] = temp_clk_oe;
+			
+
 always @* begin
     if (MOD_bi) begin
        temp_data_in = uio_in[0];
        temp_clk_in = uio_in[1];
       //  uio_in[0] <= ui_in[0];
      //   uio_in[1] <= ui_in[1];
-        uio_out[0] = temp_data_out;
-        uio_out[1] = temp_clk_out;
-        uio_oe[0] = temp_data_oe;
-        uio_oe[1] = temp_clk_oe;
-        uo_out[2] = temp_data_out;
-        uo_out[3] = temp_clk_out;
-        uo_out[4] = temp_data_oe;
-        uo_out[5] = temp_clk_oe;
+        temp_data_out2 <= temp_data_out;
+        temp_clk_out2 <= temp_clk_out;
+        temp_data_oe2 <= temp_data_oe;
+        temp_clk_oe2 <= temp_clk_oe;
+
     end
     else begin
          // Assign default values if MOD_bi is 0
         temp_data_in = ui_in[0];
         temp_clk_in = ui_in[1];
-        uo_out[2] = temp_data_out;
-        uo_out[3] = temp_clk_out;
-        uo_out[4] = temp_data_oe;
-        uo_out[5] = temp_clk_oe;
-        uio_out[0] = 0;
-        uio_out[1] = 0;
-        uio_oe[0] = 0;
-        uio_oe[1] = 0;
+        temp_data_out2 <= 0;
+        temp_clk_out2 <= 0;
+        temp_data_oe2 <= 0;
+        temp_clk_oe2 <= 0;
     end
 end
+
     
   assign uo_out[6]  = 0;
   assign uo_out[7]  = 0;
